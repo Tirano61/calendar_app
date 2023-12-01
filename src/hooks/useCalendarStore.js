0774c5fd47ff4cd22f25df495b59cc15ce214932
Dario <dramirez@balanzashook.com.ar>
2023-12-01 15:55:21 -0300

    Listo arreglado

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -55,9 +55,17 @@ export const useCalendarStore = () => {
     
   }
 
-  const startDeleteEvent = () => {
-    dispatch( onDeleteEvent() );
+  const startDeleteEvent = async() => {
+    try {
+      await calendarApi.delete(`/events/${ activeEvent.id }`);
+      
+      dispatch( onDeleteEvent() );
+    } catch (error) {
+      console.log(error);
+      Swal.fire('Error al eliminar',error.response.data.msg, 'error');
+    }
   }
+
   const startLoadingEvents = async() =>{
     try {
       const { data } = await calendarApi.get('/events');
